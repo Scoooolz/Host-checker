af1ec37a7207dd4adb9fdbf96b33eba791707f87
Scoooolzs <bagasdwiferdian8989@gmail.com>
2021-12-12 15:44:55 +0700

    New releases: `1.1.0`

--- example.js
+++ example.js
@@ -1,19 +1,26 @@
-const { isGlitch, isReplit, isGithub } = require('host-checker')
+const { IsHosting, UsingHosting, version } = require("./index");
 
-if (isGlitch) {
-  console.log('This user is using Glitch.')
+// Check host
+if (IsHosting("replit")) {
+	console.log("This user is using Replit.");
 } else {
-  console.log('This user is NOT using Glitch.')
+	console.log("This user is NOT using Replit.");
 }
 
-if (isReplit) {
-  console.log('This user is using Replit.')
+if (IsHosting("github")) {
+	console.log("This user is using GitHub.");
 } else {
-  console.log('This user is NOT using Replit.')
+	console.log("This user is NOT using Github");
 }
 
-if(isGithub) {
-  console.log('This user is using Github')
+if (IsHosting("glitch")) {
+	console.log("This user is using Glitch.");
 } else {
-  console.log('This user is NOT using Github')
+	console.log("This user is NOT using Glitch.");
 }
+
+// See Host
+console.log(UsingHosting()) // if user is not using a hosting. it return null.
+
+// See Version of Host Checker Package
+console.log(`Host Checker package version: ${version}`);
